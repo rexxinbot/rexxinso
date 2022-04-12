e9807a2060239e0f0a3a2dc45b80683740477454
rexxinbot <twiliogkjls@gmail.com>
2022-04-12 08:35:32 +0700

    jewan konbek

---
+++ shop/donasi.js
@@ -0,0 +1,16 @@
+const donasibot = () => {
+return `DONASI BY REXXIN
+
+
+1. DANA
+	ㅁ 0813 1754 9796
+	
+2. GOPAY
+    ㅁ 0813 1754 9796
+	
+Semuanya atas nama ******
+`
+	}
+
+exports.donasibot = donasibot
+ 
---
+++ shop/sewa.js
@@ -0,0 +1,22 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ PERMINGGU = 3.000
+⬡ PERBULAN = 5.000
+⬡ PERMANEN = 12.000
+⬡ PREMIUM = 2.000
+
+⬡ PULSA PERMANEN + PREM = 15.000
+
+
+⬣ MINAT? PM
+⬡ wa.me/6281317549796?text=Min+Sewa+Bot
+
+
+NOTE : 
+Bot On 24 Jam Jika Tidak Ada Kesalahan Pada Jaringan `
+	}
+
+exports.pc_sewa = pc_sewa
